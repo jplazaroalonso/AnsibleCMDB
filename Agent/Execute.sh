a5adb8b1de3c8a84578d70dffc38cabf11134bce
Juan Pablo Lazaro Alonso <juanpablo@lazaroalonso.com>
2021-01-08 11:54:10 +0100

    final commit

--- Agent/Execute.sh
+++ Agent/Execute.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 echo $REMOTEPASS
 cd /root/.ssh
-chmod 600 lm_root
-chmod 644 lm_root.pub
+# Ensure the correct permission of keys
+chmod 600 $GitSSHKEY
+chmod 644 $GitSSHKEY
+chmod 600 $SSHKEYANS
+chmod 644 $GitSSHKEY
+
 eval $(ssh-agent)
-ssh-add lm_root
+ssh-add $GitSSHKEY
 ssh -tt -o StrictHostKeyChecking=no github.com
 ssh-keyscan github.com > /root/.ssh/know_host
 cd /
 git clone git@github.com:jplazaroalonso/AnsibleCMDB.git
 cd /AnsibleCMDB
-ansible-playbook ./Ansible/getconfiguration.yaml -i $INVENTARIO 
-# sshpass -p $REMOTEPASS  ansible-playbook ./Ansible/getconfiguration.yaml -i $INVENTARIO -u $REMOTEUSER --ask-pass
+# If  $REMOTEPASS is the default, the script use the SSHKEY to connect with the servers in the inventory
+# The idea is not use the security conetxt on the Ansible Inventory and pass the credentials when is executed
+# the container (for example retrieve the user and pasword using a vault)
+if [ $REMOTEPASS == "NOPASS"]
+
+    eval $(ssh-agent)
+    ssh-add $SSHKEYANS
+    ansible-playbook ./Ansible/getconfiguration.yaml -i $INVENTARIO 
+else
+    # use sshpass to connect without interaction when use the --ask-pass flags
+    sshpass -p $REMOTEPASS  ansible-playbook ./Ansible/getconfiguration.yaml -i $INVENTARIO -u $REMOTEUSER --ask-pass
+fi
+eval $(ssh-agent)
+ssh-add $GitSSHKEY
+# Send to Git repository the changes
 git add -A
-git config --global user.email "juanpablo@lazaroalonso.com"
-git config --global user.name "jplazaro"
+# Change the next lines with your git data 
+git config --global user.email "xxxx@domain.com"
+git config --global user.name "gitusr"
 git commit -m "Actualize Inventory"
 git push origin
 
