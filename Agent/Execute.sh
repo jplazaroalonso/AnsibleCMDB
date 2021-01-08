b39662132ac45d822d0cefeda3890dc821134c0b
Juan Pablo Lazaro Alonso <juanpablo@lazaroalonso.com>
2021-01-08 10:42:41 +0100

    commit

--- Agent/Execute.sh
+++ Agent/Execute.sh
@@ -9,9 +9,11 @@ ssh -tt -o StrictHostKeyChecking=no github.com
 ssh-keyscan github.com > /root/.ssh/know_host
 cd /
 git clone git@github.com:jplazaroalonso/AnsibleCMDB.git
+cd /AnsibleCMDB
 sshpass -p $REMOTEPASS  ansible-playbook ./Ansible/getconfiguration.yaml -i $INVENTARIO -u $REMOTEUSER --ask-pass
 git add -A
 git config --global user.email "juanpablo@lazaroalonso.com"
 git config --global user.name "jplazaro"
 git commit -m "Actualize Inventory"
 git push origin
+
